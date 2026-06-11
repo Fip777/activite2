60a9044fe34ec0a09c26134e6b7f09ad1be34c76
Fip777 <first.prod1000@hotmail.com>
2026-06-11 16:10:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+/* =============
+   COPYRIGHT
+================
+Copyright (C) 2026, Audrey Bourgeois. Tous droits réservés. 
+Les informations contenues ici, code et concepts intellectuels, sont la propriété de Audrey Bourgeois 
+et sont protégés par le droit d'auteur. 
+Toute diffusion ou reproduction même partielle, quel qu’en soit le support, est interdite.*/
+
 const home = document.getElementById("home");
 const activity = document.getElementById("activity");
 const instructionEl = document.getElementById("instruction");
@@ -7,98 +15,32 @@ const finishBtn = document.getElementById("finish");
 // Données des activités
 const activities = {
   activite1: {
-    text: "Trouvez des points communs entre [liste] et [liste].",
-    words: [
-	"un chien", "un chat", "un lion", "un tigre", "un éléphant",
-     "une girafe", "un zèbre", "un cheval", "une vache", "un mouton",
-     "une chèvre", "un cochon", "un lapin", "une souris", "un rat",
-     "un ours", "un loup", "un renard", "un singe", "un gorille",
-     "un requin", "un dauphin", "une baleine", "un poisson", "une pieuvre"
-	  ]
-  },
+    text: "Description détaillée."
+     },
+  
   activite2: {
-    text: "Imaginez ce qui se passerait si [liste1] avait le pouvoir de [liste2].",
-    lists: {
-		liste1:[
-		"pomme", "banane", "orange", "fraise", "framboise",
-		"cerise", "poire", "kiwi", "mangue", "ananas",
-		"raisin", "melon", "pastèque", "citron", "abricot"
-		],
-		liste2:[
-		"un chien", "un chat", "un lion", "un tigre", "un éléphant",
-		"une girafe", "un zèbre", "un cheval", "une vache", "un mouton",
-		"une chèvre", "un cochon", "un lapin", "une souris", "un rat",
-		"un ours", "un loup", "un renard", "un singe", "un gorille",
-		"un requin", "un dauphin", "une baleine", "un poisson", "une pieuvre"
-		]
-	}
-  },
+      text: "Description détaillée."
+     },
+  
   activite3: {
-    text: "Trouvez des manières d'utiliser [liste] <br>(pour un usage totalement différent de sa fonction initiale).",
-    words: [
-      "pomme", "banane", "orange", "fraise", "framboise",
-      "cerise", "poire", "kiwi", "mangue", "ananas",
-      "raisin", "melon", "pastèque", "citron", "abricot"
-    ]
-  },
+      text: "Description détaillée."
+     },
+  
   activite4: {
-    text: "Posez-vous des questions inattendues sur : [liste].",
-    words: [
-      "pomme", "banane", "orange", "fraise", "framboise",
-      "cerise", "poire", "kiwi", "mangue", "ananas",
-      "raisin", "melon", "pastèque", "citron", "abricot"
-    ]
-  },
+      text: "Description détaillée."
+     },
+   
   activite5: {
-    text: "Combinez [liste1] avec [liste2], qu'est-ce que cela donnerait ? ",
-    lists: {
-		liste1:[
-		"un chien", "un chat", "un lion", "un tigre", "un éléphant",
-		"une girafe", "un zèbre", "un cheval", "une vache", "un mouton",
-		"une chèvre", "un cochon", "un lapin", "une souris", "un rat",
-		"un ours", "un loup", "un renard", "un singe", "un gorille",
-		"un requin", "un dauphin", "une baleine", "un poisson", "une pieuvre"
-				],
-		liste2:[
-		"pomme", "banane", "orange", "fraise", "framboise",
-		"cerise", "poire", "kiwi", "mangue", "ananas",
-		"raisin", "melon", "pastèque", "citron", "abricot"
-		]
-	}
-  },
-  activite6: {
-    text: "Comment [liste1] résoudrait la situation suivante: [liste2]? ",
-    lists: {
-		liste1:[
-		"un chien", "un chat", "un lion", "un tigre", "un éléphant",
-		"une girafe", "un zèbre", "un cheval", "une vache", "un mouton",
-		"une chèvre", "un cochon", "un lapin", "une souris", "un rat",
-		"un ours", "un loup", "un renard", "un singe", "un gorille",
-		"un requin", "un dauphin", "une baleine", "un poisson", "une pieuvre"
-				],
-		liste2:[
-		"pomme", "banane", "orange", "fraise", "framboise",
-		"cerise", "poire", "kiwi", "mangue", "ananas",
-		"raisin", "melon", "pastèque", "citron", "abricot"
-		]
-	}
-  },
+      text: "Description détaillée."
+     },
+  
+   activite6: {
+      text: "Description détaillée."
+     },
+   
   activite7: {
-    text: "Que se passerait-il si l'inverse de cette situation était vrai ou obligatoire: [liste]?",
-    words: [
-      "pomme", "banane", "orange", "fraise", "framboise",
-      "cerise", "poire", "kiwi", "mangue", "ananas",
-      "raisin", "melon", "pastèque", "citron", "abricot"
-    ]
-  },
-  activite8: {
-    text: "Décrivez ce que vous feriez si cette situation devenait la norme: [liste].",
-    words: [
-      "pomme", "banane", "orange", "fraise", "framboise",
-      "cerise", "poire", "kiwi", "mangue", "ananas",
-      "raisin", "melon", "pastèque", "citron", "abricot"
-    ]
-  }
+      text: "Description détaillée."
+      }
 };
 
 
